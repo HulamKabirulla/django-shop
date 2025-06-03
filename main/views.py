d5f87d5b12697b132cbd0250279b6340824097e6
Kabirulla Hulam <kabirullahulam@Kabirullas-MacBook-Pro.local>
2025-06-03 19:44:09 +0100

    single product.html

--- main/views.py
+++ main/views.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+from contextlib import nullcontext
+
 from django.http import HttpResponse
 from django.shortcuts import render
 from product.models import (Category, Subcategory, Product,
@@ -17,10 +20,26 @@ def singleProduct(request, productUrl):
     categories = Category.objects.prefetch_related('subcategories')
     product = (Product.objects.prefetch_related('images').
                filter(url=productUrl).order_by('images__is_main'))
-    productFilters = ProductFilters.objects.get(product_id=product.first().id)
-    print(productFilters.subcategoryFiltersValues.name)
+    productFilters = ProductFilters.objects.filter(product_id=product.first().id).order_by('subcategoryFilters_id')
+
+    productFiltersNew = defaultdict(list)
+
+    groupByValue = 0
+    index = 0
+    for productFilter in productFilters:
+        if productFilter == productFilters[0]:
+            productFiltersNew[groupByValue].append(productFilter)
+        elif productFilter.subcategoryFilters.name == productFilters[index-1].subcategoryFilters.name:
+            productFiltersNew[groupByValue].append(productFilter)
+        else:
+            groupByValue += 1
+            productFiltersNew[groupByValue].append(productFilter)
+        index += 1
+    print(productFiltersNew[0][1].subcategoryFiltersValues.name)
+    # print(productFilters[0].subcategoryFiltersValues.name)
+    # print(productFilters[0].subcategoryFilters.name)
     return render(request, 'main/detail.html', {'categories': categories,
-                                                'product': product})
+                                                'product': product, 'productFilters': dict(productFiltersNew)})
 
 def productsbycategory(request, category):
     categories = Category.objects.prefetch_related('subcategories')
